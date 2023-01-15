4586a59ad8cde2c8a5539fef14fb449aa4c624de
admiral41 <kafle.susan671@outlook.com>
2023-01-15 10:20:34 +0545

    :hammer: added images in splashscreen

--- lib/Screens/SplashScreen.dart
+++ lib/Screens/SplashScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/my_theme.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
@@ -11,7 +13,12 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      backgroundColor: MyTheme.splash,
+      body: Container(
+        child: Center(
+          child: Image.asset("assets/images/splash_icon.png"),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ticketplease/utils/my_theme.dart';
 
 import 'Screens/SplashScreen.dart';
 
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: MyTheme.myLightTheme,
+      debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
   }
