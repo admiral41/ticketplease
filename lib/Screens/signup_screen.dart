b13aa52feb952d5c4d00058202a878ef509c1e7b
admiral41 <kafle.susan671@outlook.com>
2023-01-15 12:11:50 +0545

    :sparkles: Signup Screen init

--- lib/Screens/signup_screen.dart
+++ lib/Screens/signup_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import '../utils/my_theme.dart';
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({Key? key}) : super(key: key);
 
@@ -9,6 +11,20 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
+    final Size _size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: MyTheme.splash,
+      body: Container(
+        height: _size.height,
+        width: _size.width,
+        child: Column(
+          children: [
+            Image.asset("assets/images/splash_icon.png",
+              height: 100,
+              width: 800,),
+          ],
+        ),
+      ),
+    );
   }
 }
