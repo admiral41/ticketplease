f4b7d1013a5b821ae6d188048ce4928132c803fe
admiral41 <kafle.susan671@outlook.com>
2023-01-15 10:35:21 +0545

    :hammer: added bouncing animation and linked with loginscreen

--- lib/Screens/SplashScreen.dart
+++ lib/Screens/SplashScreen.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import '../utils/my_theme.dart';
+import 'login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,14 +11,41 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 1000));
+    _animation = CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.bounceOut,
+        reverseCurve: Curves.bounceInOut);
+    _animationController.forward();
+    Timer(const Duration(milliseconds: 2500), () => Navigator.pushReplacement(context,MaterialPageRoute(builder: (_)=> LoginScreen())));
+     super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    // TODO: implement dispose
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return Scaffold(
       backgroundColor: MyTheme.splash,
       body: Container(
         child: Center(
-          child: Image.asset("assets/images/splash_icon.png"),
+          child: ScaleTransition(
+              scale: _animation,
+              child: Image.asset("assets/images/splash_icon.png")),
         ),
       ),
     );
