ae0050a3793094212f9fe705f7267cd423de6419
admiral41 <kafle.susan671@outlook.com>
2023-01-22 10:19:51 +0545

    :sparkles: Homepage

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,7 +1,9 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:ticketplease/Controller/auth_controller.dart';
+import 'package:ticketplease/utils/my_theme.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,16 +15,13 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            AuthController.instance.signOut();
-          },
-          child: Center(
-            child: Text("Logout"),
-          ),
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(statusBarColor: MyTheme.statusBar));
+    return  SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          leading: CachedNetworkImage,
         ),
+
       ),
     );
   }
