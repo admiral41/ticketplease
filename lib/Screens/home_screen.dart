3cacca93f587eb196aaefaee28a2749714f4a094
admiral41 <kafle.susan671@outlook.com>
2023-01-22 11:33:29 +0545

    :hammer: aaded icons and added search and notification in appbar

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:ticketplease/Controller/auth_controller.dart';
 import 'package:ticketplease/utils/constants.dart';
 import '../utils/dummy_data.dart';
@@ -27,45 +28,55 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: PreferredSize(
           preferredSize: const Size.fromHeight(60),
           child: AppBar(
-              leading: Padding(
-                padding: const EdgeInsets.only(top: 8, left: 8),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(30),
-                  child: CachedNetworkImage(
-                    fit: BoxFit.cover,
-                    imageUrl: picUrl,
-                    height: 60,
-                    width: 60,
-                  ),
+            leading: Padding(
+              padding: const EdgeInsets.only(top: 8, left: 8),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: CachedNetworkImage(
+                  fit: BoxFit.cover,
+                  imageUrl: picUrl,
+                  height: 60,
+                  width: 60,
                 ),
               ),
-              title: Padding(
-                padding: const EdgeInsets.only(left: 5),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Name"),
-                    DropdownButton<String>(
-                        value: city,
-                        dropdownColor: MyTheme.statusBar,
-                        isDense: true,
-                        icon: Icon(Icons.keyboard_arrow_down,color: Colors.white.withOpacity(0.8)),
-                        items: cities
-                            .map(
-                                (e) => DropdownMenuItem<String>(
-                                  value: e,
-                                  child: Text(e),
-                                ),
-                        )
-                            .toList(),
-                        onChanged: (st) {
-                          setState(() {
-                            city = st!;
-                          });
-                        }),
-                  ],
-                ),
-              )),
+            ),
+            title: Padding(
+              padding: const EdgeInsets.only(left: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("Name"),
+                  DropdownButton<String>(
+                      value: city,
+                      dropdownColor: MyTheme.statusBar,
+                      isDense: true,
+                      icon: Icon(Icons.keyboard_arrow_down,
+                          color: Colors.white.withOpacity(0.8)),
+                      items: cities
+                          .map(
+                            (e) => DropdownMenuItem<String>(
+                              value: e,
+                              child: Text(e),
+                            ),
+                          )
+                          .toList(),
+                      onChanged: (st) {
+                        setState(() {
+                          city = st!;
+                        });
+                      }),
+                ],
+              ),
+            ),
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: SvgPicture.asset("assets/icons/search.svg")),
+              IconButton(
+                  onPressed: () {},
+                  icon: SvgPicture.asset("assets/icons/notification.svg")),
+            ],
+          ),
         ),
       ),
     );
