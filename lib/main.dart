c7f6076949c019d2ec250ed0da75c4a7c883d4dc
admiral41 <kafle.susan671@outlook.com>
2023-01-22 09:24:34 +0545

    :card_file_box: created auth Controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:ticketplease/utils/my_theme.dart';
 
+import 'Controller/auth_controller.dart';
 import 'Screens/SplashScreen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Get.put(AuthController());
   runApp(MyApp());
 }
 
